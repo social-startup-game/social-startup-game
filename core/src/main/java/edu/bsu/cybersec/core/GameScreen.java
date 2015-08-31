53fbb018129d5c4e524b68208d44a14722c7e988
Paul Gestwicki <pvgestwicki@bsu.edu>
2015-08-31 14:20:23 -0400

    Add task selection menu for the one employee.
    The employee can be idle or developing.

--- core/src/main/java/edu/bsu/cybersec/core/GameScreen.java
+++ core/src/main/java/edu/bsu/cybersec/core/GameScreen.java
@@ -8,15 +8,19 @@ import tripleplay.entity.System;
 import tripleplay.game.ScreenStack;
 import tripleplay.ui.*;
 import tripleplay.ui.layout.AbsoluteLayout;
+import tripleplay.ui.layout.AxisLayout;
+import tripleplay.ui.util.BoxPoint;
 import tripleplay.util.Colors;
 
 import java.text.SimpleDateFormat;
 import java.util.GregorianCalendar;
 
 import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkState;
 
 public class GameScreen extends ScreenStack.UIScreen {
     private static final float SECONDS_PER_HOUR = 60 * 60;
+    private static final char DOWN_ARROW = '\u25BC';
     private int companyId;
     private GameWorld.Systematized world = new GameWorld.Systematized() {
         @SuppressWarnings("unused")
@@ -85,6 +89,8 @@ public class GameScreen extends ScreenStack.UIScreen {
         };
     };
 
+    private Entity developer;
+
     private final Label timeLabel = new Label("").addStyles(Style.COLOR.is(Colors.WHITE));
     private final Label usersLabel = new Label("").addStyles(Style.COLOR.is(Colors.WHITE));
     private final Label progressLabel = new Label("").addStyles(Style.COLOR.is(Colors.WHITE));
@@ -125,9 +131,9 @@ public class GameScreen extends ScreenStack.UIScreen {
     }
 
     private void makeDeveloper() {
-        Entity developer = world.create(true)
+        developer = world.create(true)
                 .add(world.developmentSkill, world.tasked);
-        world.tasked.set(developer.id, Task.DEVELOPMENT);
+        world.tasked.set(developer.id, Task.IDLE);
         world.developmentSkill.set(developer.id, 5);
     }
 
@@ -169,13 +175,75 @@ public class GameScreen extends ScreenStack.UIScreen {
         root.add(AbsoluteLayout.at(usersLabel, 50, 100));
         root.add(AbsoluteLayout.at(progressLabel, 50, 150));
         root.add(AbsoluteLayout.at(pauseButton, 400, 50));
+        root.add(AbsoluteLayout.at(new TaskComboBox(root), 50, 200));
         root.setSize(size());
         return root;
+    }
+
+
+    private void configureDeveloperUi(final Root root) {
+
     }
 
     @Override
     public Game game() {
         return SimGame.game;
+    }
+
+    class TaskComboBox extends Button {
+        private final int entityId;
+
+        TaskComboBox(Root root) {
+            super("Select a task " + DOWN_ARROW);
+            checkState(developer != null);
+            this.entityId = developer.id;
+            final MenuHost menuHost = new MenuHost(iface, root);
+            BoxPoint popUnder = new BoxPoint(0, 1, 0, 2);
+            addStyles(MenuHost.TRIGGER_POINT.is(MenuHost.relative(popUnder)));
+            onClick(new Slot<Button>() {
+                final String IDLE = "Idle";
+                final String DEVELOPING = "Developing";
+
+                @Override
+                public void onEmit(Button button) {
+                    MenuHost.Pop pop = new MenuHost.Pop(button,
+                            createMenu(IDLE, DEVELOPING));
+                    pop.menu.itemTriggered().connect(updater(button));
+                    menuHost.popup(pop);
+                }
+
+                private Slot<MenuItem> updater(final Button button) {
+                    return new Slot<MenuItem>() {
+                        @Override
+                        public void onEmit(MenuItem menuItem) {
+                            button.text.update(menuItem.text.get() + " " + DOWN_ARROW);
+                            world.tasked.set(developer.id, lookupTaskId(menuItem.text.get()));
+                            developer.didChange();
+                        }
+
+                        private Task lookupTaskId(String s) {
+                            switch (s) {
+                                case IDLE:
+                                    return Task.IDLE;
+                                case DEVELOPING:
+                                    return Task.DEVELOPMENT;
+                                default:
+                                    throw new IllegalArgumentException("Unexpected task " + s);
+                            }
+                        }
+                    };
+                }
+
+                private Menu createMenu(String... options) {
+                    Menu menu = new Menu(AxisLayout.vertical().offStretch().gap(3));
+                    for (String item : options) {
+                        menu.add(new MenuItem(item));
+                    }
+                    return menu;
+                }
+            });
+        }
+
     }
 
 }
