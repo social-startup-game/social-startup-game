dd6e5f33491dfb91491105e0017a8d3d7a3482cf
Paul Gestwicki <pvgestwicki@bsu.edu>
2015-09-04 15:54:13 -0400

    Provide platform-specific date/time formatter for HTML module.

---
+++ html/src/main/java/edu/bsu/cybersec/html/GWTDateFormatter.java
@@ -0,0 +1,21 @@
+package edu.bsu.cybersec.html;
+
+import com.google.gwt.core.client.JsDate;
+import com.google.gwt.i18n.client.DateTimeFormat;
+import edu.bsu.cybersec.core.PlatformSpecificDateFormatter;
+
+public class GWTDateFormatter implements PlatformSpecificDateFormatter {
+    private final DateTimeFormat format = DateTimeFormat.getFormat("EEE, d MMM yyyy hh:mm:ss aaa");
+
+
+    @Override
+    public long now() {
+        return (long) JsDate.now();
+    }
+
+    @Override
+    public String format(long ms) {
+        java.util.Date javaDate = new java.util.Date(ms);
+        return format.format(javaDate);
+    }
+}
--- html/src/main/java/edu/bsu/cybersec/html/SimGameHtml.java
+++ html/src/main/java/edu/bsu/cybersec/html/SimGameHtml.java
@@ -1,8 +1,9 @@
 package edu.bsu.cybersec.html;
 
 import com.google.gwt.core.client.EntryPoint;
-import playn.html.HtmlPlatform;
+import edu.bsu.cybersec.core.ClockUtils;
 import edu.bsu.cybersec.core.SimGame;
+import playn.html.HtmlPlatform;
 
 public class SimGameHtml implements EntryPoint {
 
@@ -12,6 +13,7 @@ public class SimGameHtml implements EntryPoint {
         // use config to customize the HTML platform, if needed
         HtmlPlatform plat = new HtmlPlatform(config);
         plat.assets().setPathPrefix("sim/");
+        ClockUtils.formatter = new GWTDateFormatter();
         new SimGame(plat);
         plat.start();
     }
