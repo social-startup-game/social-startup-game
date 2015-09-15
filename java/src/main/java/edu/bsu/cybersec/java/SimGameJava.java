0de6713ea9a8168fb1a34962be266511b204d717
Paul Gestwicki <pvgestwicki@bsu.edu>
2015-09-15 11:45:23 -0400

    Support command-line specification of window size.
    Pass "-size" with an argument in form WxH to change the size from the default. For example, use "-size 300x200" for a 300 wide, 200 tall window.

--- java/src/main/java/edu/bsu/cybersec/java/SimGameJava.java
+++ java/src/main/java/edu/bsu/cybersec/java/SimGameJava.java
@@ -1,15 +1,69 @@
 package edu.bsu.cybersec.java;
 
 import edu.bsu.cybersec.core.SimGame;
+import org.apache.commons.cli.*;
+import playn.java.JavaPlatform;
 import playn.java.LWJGLPlatform;
+import pythagoras.i.Dimension;
 
 public class SimGameJava {
 
+    private static Dimension size = new Dimension(640, 480);
+
     public static void main(String[] args) {
-        LWJGLPlatform.Config config = new LWJGLPlatform.Config();
-        // use config to customize the Java platform, if needed
+        new Parser().process(args);
+        LWJGLPlatform.Config config = createPlatformConfiguration();
         LWJGLPlatform plat = new SimGameJavaPlatform(config);
         new SimGame(plat);
         plat.start();
+    }
+
+    private static JavaPlatform.Config createPlatformConfiguration() {
+        LWJGLPlatform.Config config = new LWJGLPlatform.Config();
+        config.width = size.width;
+        config.height = size.height;
+        return config;
+    }
+
+    private static final class Parser extends BasicParser {
+        private static final String SIZE_OPTION_NAME = "size";
+        private CommandLine line;
+        private Options options = createOptions();
+
+        private static Options createOptions() {
+            @SuppressWarnings("static-access") // Static access required through CLI API
+                    Option sizeOption = OptionBuilder.withArgName("size")
+                    .hasArg()
+                    .withDescription("specify game screen size")
+                    .create(SIZE_OPTION_NAME);
+            Options options = new Options();
+            options.addOption(sizeOption);
+            return options;
+        }
+
+        public void process(String[] args) {
+            try {
+                line = parse(options, args);
+            } catch (ParseException e) {
+                System.err.println("Parsing failed. Reason: " + e.getMessage());
+                return;
+            }
+            if (line.hasOption(SIZE_OPTION_NAME)) {
+                processSizeOption(line);
+            }
+        }
+
+        private static void processSizeOption(CommandLine line) {
+            String sizeString = line.getOptionValue(SIZE_OPTION_NAME);
+            String[] separated = sizeString.split("x");
+            if (separated.length != 2) {
+                System.err.println("Illegal size expression, must be in form WxH: " + sizeString);
+            } else {
+                int w = Integer.valueOf(separated[0]);
+                int h = Integer.valueOf(separated[1]);
+                size.width = w;
+                size.height = h;
+            }
+        }
     }
 }
