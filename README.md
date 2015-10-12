c89ba0de0391796ae71ebcc5f2d59e6338a82402
Paul Gestwicki <pvgestwicki@bsu.edu>
2015-10-12 14:05:46 -0400

    Rename project and add copyright notice
    This is done in preparation for making the github project public.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Single-player cybersecurity education simulation game
+# The Social Startup Game
 This is a strategy game about managing workers in a fictional social media startup. These are the key features:
 * Authentic cybersecurity language
 * Exposes educational paths toward careers in cybersecurity
--- core/src/main/java/edu/bsu/cybersec/core/UpdatingSystem.java
+++ core/src/main/java/edu/bsu/cybersec/core/UpdatingSystem.java
@@ -1,9 +1,28 @@
+/*
+ * Copyright 2015 Paul Gestwicki
+ *
+ * This file is part of The Social Startup Game
+ *
+ * The Social Startup Game is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Social Startup Game is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with The Social Startup Game.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package edu.bsu.cybersec.core;
 
 import playn.core.Clock;
 import tripleplay.entity.Entity;
 
-import static com.google.common.base.Preconditions.*;
+import static com.google.common.base.Preconditions.checkNotNull;
 
 public class UpdatingSystem extends tripleplay.entity.System {
 
