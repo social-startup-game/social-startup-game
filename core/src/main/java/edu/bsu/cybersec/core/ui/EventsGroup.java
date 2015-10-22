d879aeee6e99e3655eab3f9c1b045df264279ceb
Paul Gestwicki <pvgestwicki@bsu.edu>
2015-10-22 11:31:15 -0400

    Fix missing event text defect
    The problem was that if the events screen was never shown, its size() was zero, and so all the absolute layout failed. The solution was to watch for parenting and to pull out the parent's size.

--- core/src/main/java/edu/bsu/cybersec/core/ui/EventsGroup.java
+++ core/src/main/java/edu/bsu/cybersec/core/ui/EventsGroup.java
@@ -24,6 +24,8 @@ import edu.bsu.cybersec.core.NarrativeEvent;
 import edu.bsu.cybersec.core.SimGame;
 import playn.core.Graphics;
 import playn.core.Image;
+import pythagoras.f.Dimension;
+import pythagoras.f.IDimension;
 import react.Slot;
 import tripleplay.ui.*;
 import tripleplay.ui.bgs.RoundRectBackground;
@@ -31,6 +33,8 @@ import tripleplay.ui.layout.AbsoluteLayout;
 import tripleplay.ui.layout.AxisLayout;
 import tripleplay.ui.util.BoxPoint;
 import tripleplay.util.Colors;
+
+import static com.google.common.base.Preconditions.checkState;
 
 public class EventsGroup extends InteractionAreaGroup {
     private static final Graphics graphics = SimGame.game.plat.graphics();
@@ -45,6 +49,8 @@ public class EventsGroup extends InteractionAreaGroup {
     private final GameWorld gameWorld;
     private final Label noEventsLabel = new Label("No current events.").addStyles(Style.COLOR.is(Colors.BLACK));
     private final Image eventSpeakerImage = ImageCache.instance().ADMIN;
+    private IDimension parentSize;
+    private Runnable onParented;
 
     public EventsGroup(GameWorld gameWorld) {
         super(new AbsoluteLayout());
@@ -59,14 +65,38 @@ public class EventsGroup extends InteractionAreaGroup {
         });
     }
 
-    private void post(NarrativeEvent narrativeEvent) {
+
+    @Override
+    protected void wasParented(Container<?> parent) {
+        super.wasParented(parent);
+        parentSize = parent.size();
+        checkState(!parent.size().equals(new Dimension(0, 0)),
+                "I expect parent to have non-zero size so I can lay out my content.");
+        if (onParented != null) {
+            onParented.run();
+            onParented = null;
+        }
+    }
+
+    private void post(final NarrativeEvent narrativeEvent) {
         needsAttention.update(true);
         ((GameWorld.Systematized) gameWorld).gameTimeSystem.setEnabled(false);
-        removeAll();
-        Group callout = makeCallout(narrativeEvent);
-        add(AbsoluteLayout.at(callout, 0, 0, size().width(), size().height()),
-                new Label(Icons.image(eventSpeakerImage))
-                        .setConstraint(AbsoluteLayout.uniform(BoxPoint.BR)));
+        Runnable constructEventUI = new Runnable() {
+            @Override
+            public void run() {
+                removeAll();
+                Group callout = makeCallout(narrativeEvent);
+                add(AbsoluteLayout.at(callout, 0, 0, parentSize.width(), parentSize.height()),
+                        new Label(Icons.image(eventSpeakerImage))
+                                .setConstraint(AbsoluteLayout.uniform(BoxPoint.BR)));
+            }
+        };
+        if (parentSize != null) {
+            constructEventUI.run();
+        } else {
+            checkState(onParented == null, "There is already a UI construction queued");
+            onParented = constructEventUI;
+        }
     }
 
     private Group makeCallout(NarrativeEvent narrativeEvent) {
@@ -83,8 +113,8 @@ public class EventsGroup extends InteractionAreaGroup {
                         buttonGroup,
                         new Shim(0, shimSize));
         Scroller scroller = new Scroller(content).setBehavior(Scroller.Behavior.VERTICAL)
-                .setConstraint(Constraints.fixedSize(size().width(), _size.height()));
-        return new SizableGroup(AxisLayout.vertical().offStretch(), size().width(), size().height())
+                .setConstraint(Constraints.fixedSize(parentSize.width(), parentSize.height()));
+        return new SizableGroup(AxisLayout.vertical().offStretch(), parentSize.width(), parentSize.height())
                 .add(scroller)
                 .addStyles(Style.BACKGROUND.is(CALLOUT_BACKGROUND));
     }
