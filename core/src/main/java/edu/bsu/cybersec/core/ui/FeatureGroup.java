f0cc449966e864deda7224595e9509064eb664f1
Paul Gestwicki <pvgestwicki@bsu.edu>
2015-11-24 09:41:27 -0500

    Use table layout for features and exploits

--- core/src/main/java/edu/bsu/cybersec/core/ui/FeatureGroup.java
+++ core/src/main/java/edu/bsu/cybersec/core/ui/FeatureGroup.java
@@ -19,49 +19,110 @@
 
 package edu.bsu.cybersec.core.ui;
 
+import com.google.common.collect.Maps;
 import edu.bsu.cybersec.core.GameWorld;
+import edu.bsu.cybersec.core.SimGame;
+import edu.bsu.cybersec.core.SystemPriority;
+import playn.core.Clock;
+import pythagoras.f.IDimension;
 import tripleplay.entity.Entity;
+import tripleplay.entity.System;
+import tripleplay.ui.*;
+import tripleplay.ui.layout.AxisLayout;
+import tripleplay.ui.layout.TableLayout;
+
+import java.util.Map;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
-public final class FeatureGroup extends ScrollingListInteractionAreaGroup {
+public final class FeatureGroup extends InteractionAreaGroup {
 
+    private static class ExposedColumn extends TableLayout.Column {
+        protected ExposedColumn(Style.HAlign halign, boolean stretch, float weight, float minWidth) {
+            super(halign, stretch, weight, minWidth);
+        }
+    }
+
+    private static final int AFTER_HEADER = 3;
+
+    private final Map<Integer, ProgressLabel> map = Maps.newHashMap();
+    private Group content = new Group(new TableLayout(
+            new ExposedColumn(Style.HAlign.LEFT, true, 1f, 10f),
+            new ExposedColumn(Style.HAlign.LEFT, true, 1f, 10f),
+            new ExposedColumn(Style.HAlign.RIGHT, true, 1f, 10f))
+    );
     private final GameWorld world;
 
     public FeatureGroup(final GameWorld gameWorld) {
-        super(gameWorld, gameWorld.featureNumber);
+        super(AxisLayout.vertical());
+        configureInsets();
+        content.add(new Label("FEATURE"),
+                new Label("PROGRESS"),
+                new Label("USERS PER HOUR"));
         this.world = checkNotNull(gameWorld);
+        new tripleplay.entity.System(world, SystemPriority.UI_LEVEL.value) {
+            @Override
+            protected boolean isInterested(Entity entity) {
+                return entity.has(gameWorld.featureNumber);
+            }
+
+            @Override
+            protected void update(Clock clock, System.Entities entities) {
+                super.update(clock, entities);
+                for (int i = 0, limit = entities.size(); i < limit; i++) {
+                    final int id = entities.get(i);
+                    final int featureId = gameWorld.featureNumber.get(id);
+                    if (!map.containsKey(featureId)) {
+                        ProgressLabel progressLabel = new ProgressLabel(id);
+                        map.put(featureId, progressLabel);
+                        content.add(AFTER_HEADER, new Label(String.valueOf(gameWorld.usersPerHour.get(id))));
+                        content.add(AFTER_HEADER, progressLabel);
+                        content.add(AFTER_HEADER, new Label(gameWorld.name.get(id)).addStyles(Style.HALIGN.left));
+                    } else {
+                        map.get(featureId).update();
+                    }
+                }
+            }
+        };
+    }
+
+    private void configureInsets() {
+        float horizontal = SimGame.game.plat.graphics().viewSize.height() * 0.02f;
+        addStyles(Style.BACKGROUND.is(Background.blank().inset(horizontal, 0f)));
     }
 
     @Override
-    protected ScrollingListItem createLabel(int entityId) {
-        return new FeatureLabel(entityId);
+    protected void wasParented(Container<?> parent) {
+        super.wasParented(parent);
+        if (isThisTheFirstParenting()) {
+            final IDimension parentSize = parent.size();
+            Scroller scroller = new Scroller(content)
+                    .setBehavior(Scroller.Behavior.VERTICAL)
+                    .setConstraint(Constraints.fixedSize(parentSize.width(), parentSize.height()));
+            add(scroller);
+        }
     }
 
-    private final class FeatureLabel extends ScrollingListItem {
+    private boolean isThisTheFirstParenting() {
+        return childCount() == 0;
+    }
 
-        private boolean hasCompletedText = false;
 
-        FeatureLabel(int entityId) {
-            super(entityId);
+    private final class ProgressLabel extends Label {
+
+        private final int entityId;
+
+        ProgressLabel(int entityId) {
+            this.entityId = entityId;
         }
 
-        @Override
-        public void update(int entityId) {
+        public void update() {
             Entity entity = world.entity(entityId);
-            final int number = world.featureNumber.get(entityId);
-            final String numberAndName = number + ": " + world.name.get(entityId);
             if (entity.has(world.developmentProgress)) {
                 final int progress = (int) (world.developmentProgress.get(entityId) / world.goal.get(entityId) * 100);
-                text.update(numberAndName + " [" + progress + "%]");
-            } else if (!hasCompletedText) {
-                final float usersPerHour = world.usersPerHour.get(entityId);
-                if (number % 10 == 0) {
-                    text.update(numberAndName + " - " + (int) usersPerHour + "  Users Per Hour");
-                } else {
-                    text.update(numberAndName + " - " + (int) usersPerHour);
-                }
-                hasCompletedText = true;
+                text.update(progress + "%");
+            } else {
+                text.update("100%");
             }
         }
     }
